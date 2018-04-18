4eee4b8c55a36707f987c066c36c98317ec07236
DANIEL-FG\o9708 <o97082@yahoo.com.tw>
2018-04-18 16:36:14 +0800

    Added InitialKick.cs and ShowStats.cs and also a cylinder

---
+++ Assets/InitialKick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialKick : MonoBehaviour
+{
+    public Vector3 angularVelocity;
+
+    private Rigidbody rigidBody;
+    private void OnEnable()  //使用OnEnable()可以確保Script執行順序不會影響  會優先進行  OnEnable()優先執行於Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.angularVelocity = angularVelocity;
+    }
+}
---
+++ Assets/ShowStats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//加入此行後即使是在編輯模式中  只要Scene有變動就會跑update
+[ExecuteInEditMode]
+public class ShowStats : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+        Debug.Log(rigidBody.inertiaTensor);
+	}
+}
